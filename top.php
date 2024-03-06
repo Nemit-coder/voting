c7f55ec368e7a260d61b7581e9332e92c6159cfe
nemit <nemitsagar558@gmail.com>
2024-03-06 12:27:52 +0530

    Changes

--- top.php
+++ top.php
@@ -2,7 +2,6 @@
 
 
 
-
 ?>
 <nav class="navbar navbar-light navbar-glass navbar-top navbar-expand-lg">
           <button class="btn navbar-toggler-humburger-icon navbar-toggler me-1 me-sm-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarStandard" aria-controls="navbarStandard" aria-expanded="false" aria-label="Toggle Navigation"><span class="navbar-toggle-icon"><span class="toggle-line"></span></span></button>
@@ -36,13 +35,8 @@
               </a>
               <div class="dropdown-menu dropdown-caret dropdown-caret dropdown-menu-end py-0" aria-labelledby="navbarDropdownUser">
                 <div class="bg-white dark__bg-1000 rounded-2 py-2">
-                  <a class="dropdown-item fw-bold text-warning" href="#!"><?php echo $_SESSION['first_name'] .$_SESSION['last_name']?></span></a>
+                  <a class="dropdown-item fw-bold text-warning" href="#!"><?php echo $_SESSION['first_name'] ?></span></a>
                   <div class="dropdown-divider"></div>
-                  <a class="dropdown-item" href="#!">Set status</a>
-                  <a class="dropdown-item" href="<?php echo BASE_URL?>pages/user/profile.html">Profile &amp; account</a>
-                  <a class="dropdown-item" href="#!">Feedback</a>
-                  <div class="dropdown-divider"></div>
-                  <a class="dropdown-item" href="<?php echo BASE_URL?>pages/user/settings.html">Settings</a>
                   <a class="dropdown-item" href="<?php echo BASE_URL?>logout.php">Logout</a>
                 </div>
               </div>
